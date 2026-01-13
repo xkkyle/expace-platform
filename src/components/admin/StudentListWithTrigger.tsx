5805eab1d4be20b76355d2572ea6866620ec965a
olhkyle <itskkyle@gmail.com>
2026-01-13 22:58:52 +0900

    feat : add new course

--- src/components/admin/StudentListWithTrigger.tsx
+++ src/components/admin/StudentListWithTrigger.tsx
@@ -1,26 +1,26 @@
-"use client";
+'use client'
 
-import React from "react";
-import { Loading, CourseSelect, StudentList } from "@/components";
-import { Course, courses } from "@/constants/courses";
+import React from 'react'
+import { Loading, CourseSelect, StudentList } from '@/components'
+import { Course, courses } from '@/constants/courses'
 
 const StudentListWithTrigger = () => {
-  const [course, setCourse] = React.useState<Course>(courses[0]);
+	const [course, setCourse] = React.useState<Course>(courses[0])
 
-  return (
-    <>
-      <CourseSelect value={course} setValue={setCourse} />
-      <React.Suspense
-        fallback={
-          <div className="flex justify-center items-center w-full h-full">
-            <Loading />
-          </div>
-        }
-      >
-        <StudentList currentCourse={course} />
-      </React.Suspense>
-    </>
-  );
-};
+	return (
+		<>
+			<CourseSelect value={course} setValue={setCourse} />
+			<React.Suspense
+				fallback={
+					<div className="flex justify-center items-center w-full h-full">
+						<Loading />
+					</div>
+				}
+			>
+				<StudentList currentCourse={course} />
+			</React.Suspense>
+		</>
+	)
+}
 
-export default StudentListWithTrigger;
+export default StudentListWithTrigger
--- src/constants/courses.ts
+++ src/constants/courses.ts
@@ -1,15 +1,16 @@
-type Course = (typeof courses)[number];
+type Course = (typeof courses)[number]
 
 const courses = [
-  "2025.12 _ AutoCAD1",
-  "2025.11 _ AutoCAD2",
-  "2025.10 _ AutoCAD1",
-  "2025.09 _ AutoCAD",
-  "2025.04 ~ 05 _ SketchUp2",
-  "2025.02 ~ 03 _ SketchUp1",
-  "2024.12 ~ 2025.01 _ SketchUp",
-  "2024.10 ~ 11 _ SketchUp",
-] as const;
+	'2026.01 _ AutoCAD2',
+	'2025.12 _ AutoCAD1',
+	'2025.11 _ AutoCAD2',
+	'2025.10 _ AutoCAD1',
+	'2025.09 _ AutoCAD',
+	'2025.04 ~ 05 _ SketchUp2',
+	'2025.02 ~ 03 _ SketchUp1',
+	'2024.12 ~ 2025.01 _ SketchUp',
+	'2024.10 ~ 11 _ SketchUp',
+] as const
 
-export type { Course };
-export { courses };
+export type { Course }
+export { courses }
